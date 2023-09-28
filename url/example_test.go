24a4286a39411b0cf7b67cd2f1ef5eb04fde8ee1
Simon Baev <simon.baev@bsci.com>
2023-09-28 17:30:25 -0600

    add example for URL fields

--- url/example_test.go
+++ url/example_test.go
@@ -13,11 +13,22 @@ func ExampleURL() {
 		log.Fatal(err)
 	}
 	u.Scheme = "https"
+	fmt.Println(u)
+	// output:
+	// https://google.com:80/?q=hello
+}
+
+func ExampleURL_fields() {
+	u, _ := url.Parse("http://google.com:80/?q=hello")
 	fmt.Println(u.Port())
 	fmt.Println(u.Hostname())
-	fmt.Println(u)
+	fmt.Println(u.Scheme)
+	fmt.Println(u.Host)
+	fmt.Println(u.Path)
 	// output:
 	// 80
 	// google.com
-	// https://google.com:80/?q=hello
+	// http
+	// google.com:80
+	// /?q=hello
 }
