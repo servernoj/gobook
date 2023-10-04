6e4ad7b1674a8586f1148ddcab5520b5890da3b3
Simon Baev <simon.baev@bsci.com>
2023-10-03 18:43:20 -0600

    add: sad path testing

--- ch05/cmd/hit/hit_test.go
+++ ch05/cmd/hit/hit_test.go
@@ -51,3 +51,36 @@ func TestRunHappy(t *testing.T) {
 		})
 	}
 }
+
+func TestRunSad(t *testing.T) {
+	t.Parallel()
+	tests := map[string]struct {
+		args string
+		err  string
+	}{
+		"no_url": {
+			args: "",
+			err:  `invalid value "" of URL: required`,
+		},
+		"too_much_concurrency": {
+			args: fmt.Sprintf("-c %d http://google.com", 101),
+			err:  "value of `c` must not be greater than value of `n`",
+		},
+	}
+	for name, tt := range tests {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			te := testEnv{
+				args: tt.args,
+			}
+			err := te.run()
+			if err == nil {
+				t.Fatal("error not reported")
+			}
+			if got, want := err.Error(), tt.err; !strings.Contains(got, want) {
+				t.Fatalf("Want %q to be part of %q\n", want, got)
+			}
+		})
+	}
+}
